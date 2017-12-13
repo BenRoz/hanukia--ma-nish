a893231d43656f66e23c8a800b006d7f1f2caf78
dbo1093 <dbo1093@gmail.com>
2017-12-13 15:16:47 +0200

    changed html

--- js/hanuqia.js
+++ js/hanuqia.js
@@ -3,12 +3,12 @@ var hanukia = {
     {
     name: "candle1",
     color: 'yellow',
-    isBurning:'true',
+    isBurning:'false',
     },
     {
     name: "candle2",
     color: 'yellow',
-    isBurning:'true',
+    isBurning:'false',
     },
     {
     name: "candle3",
@@ -23,7 +23,7 @@ var hanukia = {
     {
     name: "candle5",
     color: 'yellow',
-    isBurning:'true',
+    isBurning:'false',
     },
     {
     name: "candle6",
@@ -33,40 +33,42 @@ var hanukia = {
     {
     name: "candle7",
     color: 'yellow',
-    isBurning:'true',
+    isBurning:'false',
     },
     {
     name: "candle8",
     color: 'yellow',
-    isBurning:'true',
+    isBurning:'false',
     },
 
     ],
 };
 
- function light(){
-    for(var i=0; i<hanukia.candles.length; i++){
-      var currentCandle=hanukia.candles[i];
-      console.log(currentCandle.isBurning);
-      if (currentCandle['isBurning'] =='true'){
-      console.log(currentCandle.name);
-        document.getElementById("flame"+(i + 1)).style.backgroundColor="yellow";
-      }
-    }
+//  function light(){
+//     for(var i=0; i<hanukia.candles.length; i++){
+//       var currentCandle=hanukia.candles[i];
+//       console.log(currentCandle.isBurning);
+//       if (currentCandle['isBurning'] =='true'){
+//       console.log(currentCandle.name);
+//         document.getElementById("flame"+(i + 1)).style.backgroundColor="yellow";
+//       }
+//     }
 
-}
-light();
+// }
+// light();
 
 var start = document.getElementById('start');
-start.addEventListener('click', setInterval(startHanuka, 1000));
+start.addEventListener('click', startHanuka);
 
 var candleNumber = 0;
 function startHanuka(){
-    candleNumber = candleNumber + 1;
-    var currentCandle=hanukia.candles[candleNumber];
-    console.log(currentCandle.isBurning);
-    if (currentCandle['isBurning'] =='true'){
-    console.log(currentCandle.name);
-      document.getElementById("flame" + candleNumber).style.backgroundColor="yellow";
-}
+        setInterval(function lighting(){
+            var currentCandle = hanukia.candles[candleNumber];
+            //console.log(currentCandle.isBurning);
+            if (currentCandle['isBurning'] =='true'){
+            console.log(currentCandle.name);
+            document.getElementById("flame" + candleNumber).style.backgroundColor="yellow";
+            }
+            candleNumber = candleNumber + 1;
+    }, 3000);
 }
