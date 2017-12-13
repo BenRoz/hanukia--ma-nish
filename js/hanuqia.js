e8de0348ee4e086e693c26476998fb178de1dcb1
dbo1093 <dbo1093@gmail.com>
2017-12-13 14:41:08 +0200

    first iteration of stating hanuka function

--- js/hanuqia.js
+++ js/hanuqia.js
@@ -58,16 +58,15 @@ var hanukia = {
 light();
 
 var start = document.getElementById('start');
-start.addEventListener('click', startHanuka);
+start.addEventListener('click', setInterval(startHanuka, 1000));
 
-
-//function onPress(id){
-
-    //if (document.getElementById(id).style.backgroundColor=="yellow"){
-        //document.getElementById(id).style.backgroundColor="#FFFFFF";
-    //}
-    //else{
-        //document.getElementById(id).style.backgroundColor="yellow";
-    //}
-    //console.log(id);
-//}
+var candleNumber = 0;
+function startHanuka(){
+    candleNumber = candleNumber + 1;
+    var currentCandle=hanukia.candles[candleNumber];
+    console.log(currentCandle.isBurning);
+    if (currentCandle['isBurning'] =='true'){
+    console.log(currentCandle.name);
+      document.getElementById("flame" + candleNumber).style.backgroundColor="yellow";
+}
+}
